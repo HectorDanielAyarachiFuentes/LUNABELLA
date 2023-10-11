30e8aff6074969be6346298e8002805057f1efed
HectorDanielAyarachiFuentes <135895299+HectorDanielAyarachiFuentes@users.noreply.github.com>
2023-10-11 16:47:30 -0300

    fsd

--- lunahermosa/src/PrimerComponente.js
+++ lunahermosa/src/PrimerComponente.js
@@ -20,7 +20,7 @@ function PrimerComponente() {
       <p>Aquí puedes agregar contenido relacionado con la música.</p>
 
       <audio id="audio-element" controls>
-        <source src="ruta-al-archivo-de-audio.mp3" type="audio/mpeg" />
+        <source src="https://www.cjoint.com/doc/23_10/MJltSwoIswd_y2mate.com---Delfin-Torres-gemelas-hard-remix-con-efectos-especiales.mp3" type="audio/mpeg" />
         Tu navegador no soporta la reproducción de audio.
       </audio>
 
